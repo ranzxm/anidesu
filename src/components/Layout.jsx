edf219f874780e3b8de24cc3b9056d7c32e6a894
ranzxm <franskece3@gmail.com>
2023-10-21 17:30:13 +0700

    update bug background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import { Footer } from "./footer";
 export const Layout = ({ children }) => {
   return (
     <>
-      <div className="app">
+      <div className="app bg-black">
         <nav>
           <Navbar />
         </nav>
